89e07cf6e26ea466eecfc5891777489195d0a728
oze4 <matthewpoestreich@gmail.com>
2019-08-06 20:35:39 -0500

    Update pylasClasses.py

--- pylasClasses.py
+++ pylasClasses.py
@@ -2,8 +2,8 @@ class PylasDict(dict):
     """
     dot.notation access to dictionary attributes.
 
-    Instead of having to access attributes in a dict like obj["attribute"], this
-    class allows you to access attributes like obj.attribute.
+    Instead of having to access attributes in a dict like `obj["attribute"]`, this
+    class allows you to access attributes like `obj.attribute`.
     """
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
