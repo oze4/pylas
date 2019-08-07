d856160dd19bb7f0d277321b12bb1530c621167f
oze4 <matthewpoestreich@gmail.com>
2019-08-06 21:09:01 -0500

    Update pylasIO.py

--- pylasIO.py
+++ pylasIO.py
@@ -1,8 +1,17 @@
+import os
+
+
 def readLasFile(path:str) -> str:
-    try:
-        with open(path, 'r') as lf:
-            las = lf.read()
-        return las
-    except Exception as e:
-        raise e
+    """
+    Reads a .las file and returns as one big string.
+    """
+    file_extension = os.path.splitext(path)[1]
+    if file_extension == ".las":
+        try:
+            with open(path, 'r') as lf:
+                return lf.read()
+        except Exception as e:
+            print(e, "[readLasFile]::Error reading .las file!", repr(e))
+    else:
+        raise Exception("File supplied is not a .las file!")
 
