027916b80416c44c023cee6259add3e62110a501
oze4 <matthewpoestreich@gmail.com>
2019-08-07 08:35:15 -0500

    update

--- pylas.py
+++ pylas.py
@@ -5,4 +5,4 @@ import pylasText
 fullLasFileString = pylasIO.readLasFile("./las_files/las_with_lat_lon.las")
 
 lasSectionBlockStrings = pylasText.splitLasSectionsIntoBlockStrings(fullLasFileString)
-print(lasSectionBlockStrings["WellInformation"])
+print(lasSectionBlockStrings["VersionInformation"])
--- pylasRegex.py
+++ pylasRegex.py
@@ -1,7 +1,10 @@
 import re
 
 
-test_string_for_regex = "UWI.           37-129-28930-00-01: UNIQUE WELL ID"
+test_string_for_regex1 = "UWI.           37-129-28930-00-01: UNIQUE WELL ID"
+test_string_for_regex2 = "LOC.           Lat 40.426716--Long -79.54976: LOCATION"
+test_string_for_regex3 = "STRT.FT              8295.5000: START DEPTH"
+test_string_for_regex4 = "LOC.           Lat: 40.487717--Long: -80.733858----: LOCATION"
 
 
 def getMatch(regexResults:list) -> str:
@@ -31,3 +34,38 @@ def getMnemonicKeyFromString(string:str) -> list:
     """
     res = re.findall(r'.+?(?=\.)', string)
     return getMatch(res)
+
+
+def getMnemonicValueFromString(string:str) -> list:
+    """ 
+    Other possible solutions:
+      >>    [original solution] (?<=. ).*?(?=:)
+      >>    (?<=.) .*?:
+      >>    (?<=.) .*(?=\s*:)
+    Grabs the value of the mnemonic from a las file line. In the following example, "FT" is returned:
+    "LOC.           Lat: 40.487717--Long: -80.733858----: LOCATION"
+    """
+    res = re.findall(r"(?<=.) .*(?=\s*:)", string)
+    return getMatch(res)
+
+
+def getDescriptionFromString(string:str) -> list:
+    """
+    Grabs the description from a las file line. In the following example, "UNIQUE WELL ID" is returned:
+    "UWI.           37-129-28930-00-01: UNIQUE WELL ID"
+    """
+    res = re.findall(r'(?<=: ).*', string)
+    return getMatch(res)
+
+
+def getUnitsFromString(string:str) -> list:
+    """
+    Grabs the units (unit of measurement) from a las file line. In the following example, "FT" is returned:
+    "STRT.FT              8295.5000: START DEPTH"
+    """
+    res = re.findall(r'(?<=\.).*?(?= )', string)
+    return getMatch(res)
+
+
+k = getMnemonicValueFromString(test_string_for_regex3)
+print(k)
