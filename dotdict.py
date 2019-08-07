a99d9c84be2852306ecb24b9f3f9d34f3e754e5b
oze4 <matthewpoestreich@gmail.com>
2019-08-06 20:33:53 -0500

    Update dotdict.py

--- dotdict.py
+++ dotdict.py
@@ -1,5 +1,10 @@
-class dotdict(dict):
-    """dot.notation access to dictionary attributes"""
+class PylasDict(dict):
+    """
+    dot.notation access to dictionary attributes.
+
+    Instead of having to access attributes in a dict like obj["attribute"], this
+    class allows you to access attributes like obj.attribute.
+    """
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
